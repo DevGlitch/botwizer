e533210ddddd98fe5524db882c59fd7596b4ab0d
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2020-11-09 22:25:47 -0800

    Improved pages.py by using website icons to click on

--- final_project/actions/pages.py
+++ final_project/actions/pages.py
@@ -4,42 +4,55 @@ import os
 
 
 class Pages:
-
     @staticmethod
     def home_page():
-        # Launch Instagram Home Page
-        driver.get("https://www.instagram.com")
-        sleep(2)
-
-    @staticmethod
-    def account_page(account: str):
-        # Opens account page - yours or someones else's account page
-        driver.get(os.path.join("https://www.instagram.com/", account, "/"))
-        sleep(2)
-
-    @staticmethod
-    def inbox():
-        # Inbox
-        driver.get("https://www.instagram.com/direct/inbox/")
-        sleep(2)
-
-    @staticmethod
-    def new_message():
-        # To send a new DM
-        driver.get("https://www.instagram.com/direct/new/")
+        # Instagram Home Page
+        home_icon = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[1]/div/a'
+        )
+        home_icon.click()
         sleep(2)
 
     @staticmethod
     def explore_page():
         # Leads to explore page - no real need at this time to use
-        driver.get("https://www.instagram.com/explore/")
+        explore_icon = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[3]/a'
+        )
+        explore_icon.click()
+        # This page take longer to load
+        sleep(4)
+
+    @staticmethod
+    def account_page(account: str):
+        # Opens account page - yours or someones else's account page
+        driver.get("https://www.instagram.com/" + account + "/")
+        sleep(2)
+
+    @staticmethod
+    def inbox():
+        # Your inbox
+        inbox_icon = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[2]/a'
+        )
+        inbox_icon.click()
+        sleep(2)
+
+    @staticmethod
+    def new_message():
+        # To send a new DM
+        # should add requirement to have inbox() ran before if not then should run it   !!!!!!!!!!!!!!
+        new_dm_icon = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button'
+        )
+        new_dm_icon.click()
         sleep(2)
 
     @staticmethod
     def tag_page(keyword: str):
         # Page of a specific tag
-        tag_page = os.path.join("https://www.instagram.com/explore/tags/#", keyword, "/")
-        return tag_page
+        driver.get("https://www.instagram.com/explore/tags/" + keyword + "/")
+        sleep(2)
 
     @staticmethod
     def saved_pictures_page():
@@ -61,7 +74,7 @@ class Pages:
 
     @staticmethod
     def countries_list(country: str):
-        driver.get(os.path.join("https://www.instagram.com/explore/locations/", country, "/"))
+        driver.get("https://www.instagram.com/explore/locations/" + country + "/")
         sleep(2)
         # Lead the list of countries
         # click see more until no longer available
@@ -69,7 +82,13 @@ class Pages:
 
     @staticmethod
     def country_cities_page(country_code: str, country: str):
-        driver.get(os.path.join("https://www.instagram.com/explore/locations/", country_code, "/", country, "/"))
+        driver.get(
+            "https://www.instagram.com/explore/locations/"
+            + country_code
+            + "/"
+            + country
+            + "/"
+        )
         sleep(2)
         # This will lead to the cities of that country
         # Should retrieve full list of countries
