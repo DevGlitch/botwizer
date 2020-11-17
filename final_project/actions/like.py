6e3011a60f61454d190e79c65951f615eed4c176
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2020-11-17 00:09:36 -0800

    Fixed like + added more complexity to the function

--- final_project/actions/like.py
+++ final_project/actions/like.py
@@ -3,25 +3,30 @@ from time import sleep
 
 
 def like():
+
     like_icons = driver.find_elements_by_xpath("//*[contains(@aria-label, 'Like')]")
+    unlike_icons = driver.find_elements_by_xpath("//*[contains(@aria-label, 'Unlike')]")
 
-    for icon in like_icons:
+    for icon in unlike_icons or like_icons:
 
-        fill_color = icon.__getattribute__("fill")
-        height = icon.__getattribute__("height")
+        height = icon.get_attribute("height")
+        fill_color = icon.get_attribute("fill")
 
         # Ensuring it is the correct icon and that it has not been liked before
-        if height == "24" and fill_color == "262626":
-            # Fill color of post NOT liked is #262626
-            like_button = icon.find_element_by_xpath('..')
-            like_button.click()
-            sleep(2)
 
-        elif height == "24" and fill_color == "ed4956":
-            # Fill color of a post liked is #ed4956
+        if height == "24" and fill_color == "#ed4956":
+            # Fill color of a post already liked is #ed4956
             print("Picture already liked.")
             pass
 
+        elif height == "24" and fill_color == "#262626":
+            # Fill color of post NOT liked is #262626
+            print("Need to like")
+            # ('..') is used here to fetch the parent of icon using xpath
+            like_button = icon.find_element_by_xpath('..')
+            like_button.click()
+            print("liked the picture")
+            sleep(2)
+
         else:
-            print("Nothing to like")
             pass
