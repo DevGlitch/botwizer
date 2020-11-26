661088ce032caf30ff07db9d008888ca2760559f
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2020-11-25 23:04:31 -0800

    Implemented get_video()

--- final_project/actions/picture.py
+++ final_project/actions/picture.py
@@ -15,18 +15,6 @@ def open_first_pic():
     sleep(2)
 
 
-'''def get_src_img():
-    # Get the src link of the image opened
-    img = driver.find_element_by_xpath("//img")
-    return img.get_attribute("src")  # ISSUE it includes in url ?.... after img extension - need to clean url'''
-
-
-def get_src_video():
-    # Get the src link of the image opened
-    img = driver.find_element_by_xpath("//video")
-    return img.get_attribute("src")
-
-
 def get_img():
     # Find all elements containing img
     page_images = driver.find_elements_by_xpath("//img")
@@ -35,7 +23,7 @@ def get_img():
     for img in page_images:
         sizes = img.get_attribute("sizes")
 
-        # To find the post image
+        # To find the post's image
         if sizes > "293px":
             # 293px is the size of the post preview
             # Any picture bigger than 293px is a post picture
@@ -46,35 +34,35 @@ def get_img():
             get_filename = urlparse(url).path
 
             # Image filename
-            filename = os.path.basename(get_filename)
+            img_name = os.path.basename(get_filename)
 
             # Create folders to store the image
             os.makedirs("data/images", exist_ok=True)
 
-            # Path were the image will be saved
-            path = os.path.join("data/images/", filename)
+            # Path where the image will be saved
+            path = os.path.join(os.path.abspath("data"), "images/", img_name)
 
             # Making sure the file doesn't exist already
-            if not os.path.exists(filename):
+            if not os.path.exists(path):
 
                 # Using package requests to check for any http issue like 4XX or 5XX errors
                 with requests.get(url, stream=True) as req:
                     # Checking if request is successful (None = no error)
                     if req.raise_for_status() is not None:
-                        print("Error: URL of picture is incorrect.")
+                        print("Error: URL of picture is incorrect...")
                         pass
 
                     # Writing file atomically locally
                     with atomic_write(path, as_file=False) as f:
                         urlretrieve(url, filename=f)
-                        print("Image saved.")
+                        print("Image saved...")
 
                 # Returning the image path in order to use with other functions
                 return f
 
             else:
                 # Pass if the picture is already present in the folder
-                print("Picture already exists.")
+                print("Picture already exists...")
                 pass
 
         else:
@@ -82,9 +70,47 @@ def get_img():
 
 
 def get_vid():
-    # Download video locally
-    page_videos = driver.find_elements_by_xpath("//video")
-    ...
+    # Find the first element containing video
+    # The first element will always be the post's video
+    page_video = driver.find_element_by_xpath("//video")
+
+    # Getting the URL of the video
+    url = page_video.get_attribute("src")
+
+    # Parsing the URL in order to get the filename path
+    get_filename = urlparse(url).path
+
+    # Video filename
+    filename = os.path.basename(get_filename)
+
+    # Create folders to store the video
+    os.makedirs("data/videos", exist_ok=True)
+
+    # Path where the video will be saved
+    path = os.path.join("data/videos/", filename)
+
+    # Making sure the video doesn't exist already
+    if not os.path.exists(path):
+
+        # Using package requests to check for any http issue like 4XX or 5XX errors
+        with requests.get(url, stream=True) as req:
+            # Checking if request is successful (None = no error)
+            if req.raise_for_status() is not None:
+                print("Error: URL of video is incorrect...")
+                pass
+
+            # Writing file atomically locally
+            with atomic_write(path, as_file=False) as f:
+                urlretrieve(url, filename=f)
+                print("Video saved...")
+
+        # Returning the video path in order to use with other functions
+        return f
+
+    else:
+        # Pass if the video is already present in the folder
+        print("Video already exists...")
+        pass
 
 
 def next_pic():
